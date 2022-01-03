90701c083f0b4850e79479b08d29274363f2ac63
Elide <zavalavinagreelide@gmail.com>
2022-01-03 16:59:31 -0600

    async/await part2

--- asynchrous/index.js
+++ asynchrous/index.js
@@ -31,10 +31,34 @@ const getDogPic = async () => {
 
     await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file');
-  } catch (error) {}
+  } catch (error) {
+    console.log(err);
+    throw err;
+  }
+  return '2: READY 🐹';
 };
 
-getDogPic();
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  })
+  .catch((err) => {
+    console.log('ERROR 💥');
+  });
 /*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
